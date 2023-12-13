dbdef62bf0f3b5851d6726b2ac7b313b2865b3ed
Monika Rydstrom <87671051+monikat45@users.noreply.github.com>
2023-12-13 06:21:50 -0700

    Add files via upload

--- student-registration-app/src/app/app.module.ts
+++ student-registration-app/src/app/app.module.ts
@@ -6,7 +6,14 @@ import { AppComponent } from './app.component';
 import { StudentRegistrationComponent } from './student-registration/student-registration.component';
 import { StudentService } from './services/student.service';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -17,6 +24,15 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    FormsModule
+
+
   ],
   providers: [StudentService],
   bootstrap: [AppComponent]
